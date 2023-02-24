e683ad21a409b121d0956ed31054069d0c4a10e0
nanyoon11 <nanyoonthinzar11@gmail.com>
2023-02-24 14:43:27 +0630

    first commit

---
+++ src/main/java/com/nytz/springbootrestexample/controller/ProductController.java
@@ -0,0 +1,43 @@
+package com.nytz.springbootrestexample.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.nytz.springbootrestexample.model.Product;
+import com.nytz.springbootrestexample.service.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return service.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> findAllProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("{id}")
+    public Product findProductById(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+    @PutMapping
+    public Product updateProduct(@RequestBody Product product) {
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteProduct(@PathVariable int id) {
+        return service.deleteProduct(id);
+    }
+    
+}
